af0238a383718fb08412bf591547774bb29e8a78
BXuan694 <wbingxyz@163.com>
2022-12-07 14:25:28 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -5,9 +5,12 @@ import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
 import argparse
+import os, sys
 import numpy as np
 import time
+from PIL import Image
 cudnn.benchmark = True
 
 class AverageMeter(object):
@@ -31,9 +34,13 @@ class AverageMeter(object):
 def set_lr(optimizer_, newLr_):
     for paramGroup_ in optimizer_.param_groups:
         paramGroup_['lr'] = newLr_
+        break
 
+# 评估精度
 def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
+    """
+    计算topk准确率
+    """
     maxk = max(topk)
     batch_size = target.size(0)
 
@@ -54,54 +61,107 @@ def accuracy(output, target, topk=(1,)):
 
 # 计算warmup学习率
 def get_warmup_lr(curIter_, totalIters_, baseLr_, warmupRatio_, warmUpOption='linear'):
+    """
+    预热期初始的学习率：baseLr_*warmupRatio_，最终学习率：baseLr_，增长方式由warmUpOption决定。
+    input：
+     curIter_: int，当前iter轮次。
+     totalIters_: int，预热期总iter轮次。
+     baseLr_: float，基准学习率, 预热期结束时的学习率。
+     warmupRatio_: float，学习率预热比率，warmupRatio_*baseLr_表示第0iter的学习率。
+     warmUpOption: str，学习率增长形式。
+    output：
+     float，当前(curIter_)轮次的学习率。
+    """
+
     if warmUpOption == 'constant':
-        warmupLr = baseLr_ * warmupRatio_ 
+        warmupLr = baseLr_*warmupRatio_ 
     elif warmUpOption == 'linear':
-        k = (1 - curIter_ / totalIters_) * (1 - warmupRatio_)
-        warmupLr = baseLr_ * (1 - k)
+        # 学习率从baseLr_*warmupRatio_开始，增长至
+        k = (1 - curIter_/totalIters_) * (1-warmupRatio_)
+        warmupLr = baseLr_*(1-k)
     elif warmUpOption == 'exp':
-        k = warmupRatio_**(1 - curIter_ / totalIters_)
-        warmupLr = baseLr_ * k
+        k = warmupRatio_**(1 - curIter_/totalIters_)
+        warmupLr = baseLr_*k
+    elif warmUpOption == 'cosine':
+        pass
     return warmupLr
 
+def default_loader(path):
+    return Image.open(path).convert('RGB')
+
+# 自定义数据集类
+class MyDataset(Dataset):
+    def __init__(self, txt, transform=None, target_transform=None, loader=default_loader):
+        fh = open(txt, 'r')
+        imgs = []
+        for line in fh:
+            line = line.rstrip()
+            line = line.strip('\n')
+            line = line.rstrip()
+            words = line.split()
+            imgs.append((words[0],int(words[1])))
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        fn, label = self.imgs[index]
+        img = self.loader(fn)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img,label
+
+    def __len__(self):
+        return len(self.imgs)
 
 def train(globalStartEpoch, totalEpoches):
-    inputSize=64
+    #------------------------------------data--------------------------------
     transform_train = transforms.Compose([
-        transforms.RandomCrop(inputSize),
-        #transforms.RandomResizedCrop(inputSize, scale=(1, 1), ratio=(1, 1)),
+        transforms.RandomResizedCrop(cfg.inputShape, scale=(0.4, 1)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     transform_valid = transforms.Compose([
-        transforms.Resize([inputSize, inputSize]),
+        transforms.Resize(cfg.inputShape[0]),
+        transforms.CenterCrop(cfg.inputShape),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
 
     if cfg.dataset.structure=="default":
-        trainset = datasets.ImageFolder(cfg.dataset.train_info, transform=transform_train)
-        validset = datasets.ImageFolder(cfg.dataset.valid_info, transform=transform_valid)
-    else:
-        pass
-    batchSize = cfg.imgs_per_gpu * cfg.num_gpus
+        trainset = datasets.ImageFolder(cfg.dataset.trainRoot, transform=transform_train)
+        validset = datasets.ImageFolder(cfg.dataset.validRoot, transform=transform_valid)
+    elif cfg.dataset.structure=="txt":
+        trainset = MyDataset(txt=os.path.join(cfg.dataset.trainRoot, 'dataset-trn.txt'), transform=transform_train)
+        validset = MyDataset(txt=os.path.join(cfg.dataset.validRoot, 'dataset-val.txt'), transform=transform_valid)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchSize, shuffle=True, pin_memory=True, num_workers=cfg.workers_per_gpu)
-    validloader = torch.utils.data.DataLoader(validset, batch_size=batchSize, shuffle=False, pin_memory=True, num_workers=cfg.workers_per_gpu)
+    validloader = DataLoader(validset, batch_size=cfg.batchSize, shuffle=False, pin_memory=True, num_workers=cfg.workers_per_gpu)
+    '''
+    for idx, (data, target) in enumerate(validloader):
+        if(idx%10==0):
+            print(str(idx)+' '+str(target))
+    '''
+    #------------------------------------model--------------------------------
     model = cfg.backbone
-    if cfg.resume_from is not None:
-        model.load_state_dict(torch.load(cfg.resume_from))
-    model.cuda()
-    model.train()
     
+    # 抽取与训练权重
+    model_dict = model.state_dict()
+    model_official = cfg.model_official
+    model_official = {k: v for k, v in model_official.state_dict().items() if k in model_dict}
+    model_dict.update(model_official)
+    model.load_state_dict(model_dict)
+    model.cuda()
+
+    #------------------------------------optimizer--------------------------------
     lrOri = cfg.optimizer['lr']
     lrStages = cfg.lr_config["step"]
     lrList = np.full(totalEpoches, lrOri)
     for ii in range(len(lrStages)):
         lrList[lrStages[ii]:]*=0.1
-    print("starting epoch: ", globalStartEpoch)
+    print("Lr: ", lrList)
     print("lr adapting stages: ", end=' ')
     for ii in range(len(lrStages)):
         print(cfg.lr_config["step"][ii], end=" ")
@@ -111,8 +171,8 @@ def train(globalStartEpoch, totalEpoches):
     optimizer = torch.optim.SGD(model.parameters(), lr=optimizer_config['lr'], momentum=optimizer_config['momentum'], weight_decay=optimizer_config['weight_decay'])
     criterion = nn.CrossEntropyLoss()
 
-    batchSize = cfg.imgs_per_gpu * cfg.num_gpus
-    epochSize = len(trainset) // batchSize  
+    print("starting epoch: ", globalStartEpoch)
+    epochSize = len(trainset) // cfg.batchSize  
     # nums of trained epoches, idx of epoch to start
     pastEpoches = globalStartEpoch
     # nums of trained iters, idx of iter to start
@@ -126,8 +186,11 @@ def train(globalStartEpoch, totalEpoches):
     top1 = AverageMeter()
     top5 = AverageMeter()
     currentIter = 0
+    showIters = 10
 
+    #------------------------------------training--------------------------------
     for epoch in range(leftEpoches):
+        trainloader = DataLoader(trainset, batch_size=cfg.batchSize, shuffle=True, pin_memory=True, num_workers=cfg.workers_per_gpu)
         currentEpoch = epoch + pastEpoches
 
         # 终止训练
@@ -136,6 +199,9 @@ def train(globalStartEpoch, totalEpoches):
             return
 
         # 仅用于打印
+        model.train()
+        top1.reset()
+        top5.reset()
         for batchIdx, (inputs, targets) in enumerate(trainloader):
             iterStartTime = time.time()
 
@@ -165,7 +231,6 @@ def train(globalStartEpoch, totalEpoches):
             pastIters += 1
             currentIter += 1
 
-            showIters = 10
             if batchIdx%int(showIters) == 0 and batchIdx != 0:
                 iterLastTime = time.time() - iterStartTime
                 left_seconds = iterLastTime * leftIters
@@ -182,12 +247,26 @@ def train(globalStartEpoch, totalEpoches):
                     'Prec@5 {top5.val:.2f} ({top5.avg:.2f})\t[lr:{lr:.6g}]'.format(
                     currentEpoch, totalEpoches, batchIdx, len(trainloader),
                     loss=loss.item(), top1=top1, top5=top5, lr=cur_lr))
+            sys.stdout.flush()
 
         leftEpoches -= 1
         top1.reset()
         top5.reset()
         save_name = "./weights/" + cfg.name + "_epoch_" + str(currentEpoch) + ".pth"
         torch.save(model.state_dict(), save_name)
+
+        model.eval()
+        for batchIdx, (inputs, targets) in enumerate(validloader):
+            inputs, targets = inputs.cuda(), targets.cuda()
+            with torch.no_grad():
+                outputs = model(inputs)
+            prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+            top1.update(prec1.item(), inputs.size(0))
+            top5.update(prec5.item(), inputs.size(0))
+            if batchIdx%int(showIters) == 0 and batchIdx != 0:
+                print('validset: Prec@1 {top1.val:.2f} ({top1.avg:.2f})\t'
+                    'Prec@5 {top5.val:.2f} ({top5.avg:.2f})'.format(
+                    top1=top1, top5=top5))
 
 if __name__ == '__main__':
     train(globalStartEpoch=cfg.epoch_iters_start, totalEpoches=cfg.total_epoch)   #设置本次训练的起始epoch

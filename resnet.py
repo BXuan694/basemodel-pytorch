a7be6e352f19c3113a9213a0ae98a03e56235aaa
BXuan694 <wbingxyz@163.com>
2022-12-07 20:15:26 +0800

    Add files via upload

--- resnet.py
+++ resnet.py
@@ -70,14 +70,14 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-		#        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        #        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-		#self.linear = nn.Linear(512*block.expansion*4, numClasses)
+        #self.linear = nn.Linear(512*block.expansion*4, numClasses)
         self.linear = nn.Linear(512*block.expansion, numClasses)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -90,6 +90,7 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
+        out = F.max_pool2d(out, kernel_size=3, stride=2, padding=1)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
